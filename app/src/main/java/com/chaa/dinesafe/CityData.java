90326dce704798456defa9fa5441a49f01c04946
Amy <kimam@utschools.ca>
2017-06-04 19:42:54 -0400

    Experimenting with search methods and search view, fixed bug in Search class.

---
+++ app/src/main/java/com/chaa/dinesafe/CityData.java
@@ -0,0 +1,8 @@
+package com.chaa.dinesafe;
+
+/**
+ * Created by Amy on 2017-06-04.
+ */
+
+public class CityData {
+}
--- app/src/main/java/com/chaa/dinesafe/HomePage.java
+++ app/src/main/java/com/chaa/dinesafe/HomePage.java
@@ -2,12 +2,41 @@ package com.chaa.dinesafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
+    Button search;
+    Button nearby;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home_page);
+
+        search = (Button) findViewById(R.id.search_button);
+
+        search.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                Intent myIntent = new Intent(HomePage.this,
+                        Search.class);
+                startActivity(myIntent);
+            }
+        });
+
+        nearby = (Button) findViewById(R.id.nearby_button);
+
+        nearby.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent2 = new Intent(HomePage.this, Nearby.class);
+                startActivity(myIntent2);
+            }
+        });
     }
 }
