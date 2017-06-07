3a56ba711e7a87317a2a305aeda72785f63e011b
Amy <kimam@utschools.ca>
2017-06-06 22:21:29 -0400

    tracking

---
+++ app/src/main/java/com/chaa/dinesafe/SearchResult.java
@@ -0,0 +1,18 @@
+package com.chaa.dinesafe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Amy on 2017-06-06.
+ */
+
+public class SearchResult extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+}
